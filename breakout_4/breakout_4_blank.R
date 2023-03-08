343d8742e21cc3b7aaa794bb14167659b609f9d0
Henning Lorenzen <henning.lorenzen.ext@bayer.com>
2023-03-08 15:11:19 +0100

    complete breakout 4

--- breakout_4/breakout_4_blank.R
+++ breakout_4/breakout_4_blank.R
@@ -2,26 +2,32 @@
 # Coments with instructions will end with ----
 library(shiny)
 library(reactable)
-library(magrittr)
 library(Tplyr)
 library(dplyr)
-library(purrr)
-library(rlang)
 
 
 adsl <- readRDS(here::here('data', 'adsl.rds')) %>%
-  # Tplyr generally respects factors! 
+  # Tplyr generally respects factors!
   mutate(
     TRT01A = ordered(TRT01A, c("Placebo", "Xanomeline Low Dose", "Xanomeline High Dose"))
   )
 
 # Summarize adsl, with the treatment variable TRT01A ----
-tab <- 
-# When you're done, if you have time, edit the table above and see what else you can make, 
-# and how the table is still interactive! 
+
+tab0 <- tplyr_table(adsl, TRT01A)
+
+tab <-
+  tab0 |>
+  add_layer(group_count(SEX, by = "Sex n (%)")) |>
+  add_layer(group_desc(AGE, by = "Age (years)")) |>
+  add_layer(group_count(RACE, by = "Race n (%)"))
+
+# When you're done, if you have time, edit the table above and see what else you can make,
+# and how the table is still interactive!
 
 # Build the table with metadata ----
-results <- <build the table here> %>%
+results <-
+  build(tab, metadata = TRUE) |>
   # This is all post processing
   # See https://atorus-research.github.io/Tplyr/articles/styled-table.html
   apply_row_masks() %>%
@@ -35,23 +41,40 @@ ui <- fillPage(
 )
 
 server <- function(input, output) {
-  
+
   # Set the reactives for row and column from the click event ----
   # For row, pull out the index element from the row element of the input
   # For col, pull out the column element from the col element of the input
-  row <- 
-  col <- 
-  
-  output$demoTab <- renderReactable(
+  r_row <- reactive({
+    i <- input$row$index
+    results |>
+      magrittr::extract(i, 1) |>
+      magrittr::extract2("row_id")
+  })
+
+  r_col <- reactive({
+    input$col$column
+  })
+
+
+  output$demoTab <- renderReactable({
+    js_onclick <- JS(
+      "function(rowInfo, colInfo) {
+        if (window.Shiny) {
+          Shiny.setInputValue('row', { index: rowInfo.index + 1 })
+          Shiny.setInputValue('col', { column: colInfo.id })
+        }
+      }")
+
     reactable(
       select(results, -row_id, -starts_with("ord")),
       sortable = FALSE,
       # Set the Shiny inputs for row and column  ----
-      # For the first input value, set the input value to 'row'. For the second, set it to 'col'. 
+      # For the first input value, set the input value to 'row'. For the second, set it to 'col'.
       # Set the key for 'row' to index and 'col' to column for reference later
-      # This small JS function talks with Shiny by letting us pass in a JavaScript function directly. 
+      # This small JS function talks with Shiny by letting us pass in a JavaScript function directly.
       # Note that the data format here is built to work with reactable
-      onClick = JS(<JS Code Here>),
+      onClick = js_onclick,
       height = 450,
       defaultPageSize = 11,
       columns = list(
@@ -62,20 +85,21 @@ server <- function(input, output) {
         `var1_Xanomeline High Dose` = colDef(name = "Xano High")
       )
     )
-  )
-  
+  })
+
   # Grab the subset metadata by using the row and col reactives ----
   # Remember that the Tplyr table's object name is `tab`, and that reactives are callables! (i.e. need ())
   # row will be the row_id, and col will be the column. Use ?get_meta_subset() for help
   sub_data <- reactive({
-
+    get_meta_subset(x = tab,
+                    row_id = req(r_row()),
+                    column = req(r_col()))
   })
-  
+
   # Now we'll render the subset datable as a reactable table using the sub_data() reactive we made above
-  output$demoList<- renderReactable({
-    req(sub_data())
+  output$demoList <- renderReactable({
     reactable(
-      sub_data(),
+      data = req(sub_data()),
       sortable = FALSE,
       height = 450,
       defaultPageSize = 11,
@@ -83,5 +107,5 @@ server <- function(input, output) {
   })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
