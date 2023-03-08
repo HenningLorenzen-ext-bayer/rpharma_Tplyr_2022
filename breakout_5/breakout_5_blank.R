42c6e26c90131d5008142e669e2fe8cce77e891f
Henning Lorenzen <henning.lorenzen.ext@bayer.com>
2023-03-08 17:15:45 +0100

    complete breakout 5

--- breakout_5/breakout_5_blank.R
+++ breakout_5/breakout_5_blank.R
@@ -7,10 +7,99 @@ library(tidyr)
 library(purrr)
 library(rlang)
 
+## data loading ----
+
 adsl <- readRDS(here::here('data', 'adsl.rds'))
 adas <- readRDS(here::here('data', 'adas.rds'))
 
-# Pull your work from Breakout 3: Problem 5 to complete the application ----
+## work from breakout 3 prob. 5 ----
+
+### basic table ----
+# This table code is to support the primary efficacy table for CDISC Pilot table 14-3.01
+t <- tplyr_table(adas, TRTP, where=EFFFL == "Y" & ITTFL == "Y" & PARAMCD == "ACTOT" & ANL01FL == "Y") %>%
+  set_pop_data(adsl) %>%
+  set_pop_treat_var(TRT01P) %>%
+  set_pop_where(EFFFL == "Y" & ITTFL == "Y") %>%
+  set_distinct_by(USUBJID) %>%
+  set_desc_layer_formats(
+    'n' = f_str('xx', n),
+    'Mean (SD)' = f_str('xx.x (xx.xx)', mean, sd),
+    'Median (Range)' = f_str('xx.x (xxx;xx)', median, min, max)
+  ) %>%
+  add_layer(
+    group_desc(AVAL, where= AVISITN ==  0, by = "Baseline")
+  ) %>%
+  add_layer(
+    group_desc(AVAL, where= AVISITN == 24, by = "Week 24")
+  ) %>%
+  add_layer(
+    group_desc(CHG,  where= AVISITN == 24, by = "Change from Baseline")
+  )
+
+sum_data <- t %>%
+  build(metadata = TRUE) %>%
+  apply_row_masks() %>%
+  select(row_id, starts_with('row_label'),
+         var1_Placebo, `var1_Xanomeline Low Dose`, `var1_Xanomeline High Dose`)
+
+### efficacy mockup ----
+#### actual results ----
+# I don't need the full model code for this example so just mock it up.
+# But if you want to see it, it's available here:
+# https://github.com/RConsortium/submissions-pilot1/blob/694a207aca7e419513ffe16f6f5873526da1bdcb/R/eff_models.R#L17
+model_portion <- tibble::tribble(
+  ~"row_id",  ~"row_label1",                       ~"var1_Xanomeline Low Dose", ~"var1_Xanomeline High Dose",
+  "x4_1",    "p-value(Dose Response) [1][2]",      "",                          "0.245",
+  "x4_3",    "p-value(Xan - Placebo) [1][3]",	    "0.569",    	               "0.233",
+  "x4_4",    "   Diff of LS Means (SE)",           "-0.5 (0.82)",               "-1.0 (0.84)",
+  "x4_5",    "   95% CI",                          "(-2.1;1.1)",                "(-2.7;0.7)",
+  "x4_7",    "p-value(Xan High - Xan Low) [1][3]", "",                          "0.520",
+  "x4_8",    "   Diff of LS Means (SE)",           "",                          "-0.5 (0.84)",
+  "x4_9",    "   95% CI",                          "",                          "(-2.2;1.1)"
+)
+
+results <- bind_rows(sum_data, model_portion) %>%
+  mutate(
+    across(where(is.character), ~ replace_na(., ""))
+  )
+
+#### fixup metadata for efficacy 'results' ----
+meta <- tplyr_meta(
+  names = quos(TRTP, AVISITN, AVISIT, PARAMCD, PARAM, AVAL),
+  filters = quos(EFFFL == "Y", ITTFL == "Y", ANL01FL == "Y")
+)
+
+# Xan High / Placebo contrast
+meta_xhp <- meta |>
+  add_filters(quos(TRTP %in% c("Xanomeline High Dose", "Placebo")))
+
+# Xan Low / Placbo Contrast
+meta_xlp <- meta |>
+  add_filters(quos(TRTP %in% c("Xanomeline Low Dose", "Placebo")))
+
+# Xan High / Xan Low Contrast
+meta_xlh <- meta |>
+  add_filters(quos(TRTP %in% c("Xanomeline High Dose", "Xanomeline Low Dose")))
+
+## Problem 6 - Create a metadata dataframe and bind it to the Tplyr table's metadata
+# The metadata dataframe is pre-prepared for you
+# Use append metadata to bind it to the Tplyr tables metadata
+# View the updated metadata using get_metadata()
+eff_meta <- tibble::tribble(
+  ~"row_id",  ~"row_label1",                       ~"var1_Xanomeline Low Dose", ~"var1_Xanomeline High Dose",
+  "x4_1",    "p-value(Dose Response) [1][2]",      NULL,                        meta,
+  "x4_3",    "p-value(Xan - Placebo) [1][3]",	     meta_xlp,    	              meta_xhp,
+  "x4_4",    "   Diff of LS Means (SE)",           meta_xlp,                    meta_xhp,
+  "x4_5",    "   95% CI",                          meta_xlp,                    meta_xhp,
+  "x4_7",    "p-value(Xan High - Xan Low) [1][3]", NULL,                        meta_xlh,
+  "x4_8",    "   Diff of LS Means (SE)",           NULL,                        meta_xlh,
+  "x4_9",    "   95% CI",                          NULL,                        meta_xlh
+)
+
+
+t <- append_metadata(t, eff_meta)
+
+# shiny code ----
 
 ui <- fillPage(
   reactableOutput("demoTab"),
@@ -19,8 +108,19 @@ ui <- fillPage(
 
 server <- function(input, output) {
 
-  row <- reactive(results[input$row$index,1]$row_id)
-  col <- reactive(input$col$column)
+  r_row <- reactive({
+    i <- input$row$index
+
+    req(rlang::is_scalar_integerish(i))
+
+    results |>
+      slice(i) |>
+      pull(row_id)
+  })
+
+  r_col <- reactive({
+    input$col$column
+  })
 
   output$demoTab <- renderReactable(
     reactable(
@@ -45,19 +145,20 @@ server <- function(input, output) {
   )
 
   sub_data <- reactive({
-    req(row(), col())
-    tmp <- get_meta_subset(t, row(), col())
-    tmp
+    row_id <- req(r_row())
+    col_id <- req(r_col())
+
+    get_meta_subset(x = t, row_id = row_id, column = col_id)
   })
 
-  output$demoList<- renderReactable({
-    req(sub_data())
-    reactable(
-      sub_data(),
-      sortable = FALSE,
-      height = 450,
-      defaultPageSize = 11,
-    )
+  output$demoList <- renderReactable({
+    req(sub_data()) |>
+      reactable(
+        data = _,
+        sortable = FALSE,
+        height = 450,
+        defaultPageSize = 11,
+      )
   })
 }
 
